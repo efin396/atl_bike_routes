cabbc8dff8f9eac418a43b8362aa2573437a493d
efin396 <6finlayson@gmail.com>
2025-07-18 13:35:34 -0400

    fixed mobile responsive nav bar

--- src/navbar.js
+++ src/navbar.js
@@ -1,27 +1,29 @@
 import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
 function Navbar() {
-
   return (
-    <nav className="navbar navbar-expand-lg bg-light">
-  <div className="container-fluid">
-    {/* <a className="navbar-brand" href="/title"  >Name Here</a> */}
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
 
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#navbarNav"
-      aria-controls="navbarNav"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
+        <Link className="navbar-brand" to="/">RideApp</Link>
 
-     <div className="collapse navbar-collapse" id="navbarNav">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <Link className="nav-link active" to="">Home</Link>
+              <Link className="nav-link active" to="/">Home</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/ride-calendar">Ride Calendar</Link>
@@ -34,9 +36,8 @@ function Navbar() {
             </li>
           </ul>
         </div>
-    </div>
-</nav>
-
+      </div>
+    </nav>
   );
 }
 
